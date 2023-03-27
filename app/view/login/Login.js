8a92f12cc5a0f6786070e39285a1c0bcbe9907fc
Murad Gasanov <miuh@mail.ru>
2023-03-27 20:20:42 +0300

    Prettify login form

--- app/view/login/Login.js
+++ app/view/login/Login.js
@@ -7,33 +7,42 @@ Ext.define('LoginTestApp.view.login.Login', {
 	controller: 'login',
 
 	title: 'Окно входа',
-    bodyPadding: 10,
+    padding:'5px',
     autoShow: true,
 	closable: false,
 
 	items: {
 		xtype: 'form',
-		reference: 'form',
-		items: [{
-			xtype: 'textfield',
-			name: 'username',
-			reference: 'username',
-            fieldLabel: 'Пользователь:',
-			allowBlank: false
-		}, {
-			xtype: 'textfield',
-			name: 'password',
-            inputType: 'password',
-			reference: 'password',
-            fieldLabel: 'Пароль:',
-            allowBlank: false
-		}],
-		buttons: [{
-			text: 'Login',
-			formBind: true,
-			listeners: {
-				click: 'onLoginClick'
+		reference: 'loginform',
+		defaults:{
+			padding: '5px',
+			margin: '5px',
+		},
+		items: [
+			{
+				xtype: 'textfield',
+				name: 'username',
+				reference: 'username',
+				fieldLabel: 'Пользователь:',
+				allowBlank: false
+			},
+			{
+				xtype: 'textfield',
+				name: 'password',
+				inputType: 'password',
+				reference: 'password',
+				fieldLabel: 'Пароль:',
+				allowBlank: false
 			}
-		}]
+		],
+		buttons: [
+			{
+				text: 'Login',
+				formBind: true,
+				listeners: {
+					click: 'onLoginClick'
+				}
+			}
+		]
 	}
 });
